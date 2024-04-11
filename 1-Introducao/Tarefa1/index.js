96ebad023de19e027c7245c57d5179495f970a0b
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2024-04-11 09:18:17 -0300

    Criando tarefa para consolidar aprendizado

---
+++ 1-Introducao/Tarefa1/index.js
@@ -0,0 +1,12 @@
+//variaveis
+firstName = 'Matheus';
+secondName = 'Pereira';
+age = 20;
+profession = 'Programador Full-Stack';
+educationLevel = 'Graduando'
+university = 'PUC Minas';
+
+console.log(`Olá ${firstName}, seja bem vindo ao curso de nodeJS. Sua idade é de ${age} anos certo? Idade perfeita para aprender novas coisas.\n\n
+Vi que seu desejo de profissão é ${profession}, e nesse curso iremos te ensinar a parte do back-end para te tornar um profissional da área.
+\n\n
+Sua escolaridade atual é: ${educationLevel} pela ${university}. Tenho certeza que este curso te ajudará na jornada acadêmica também!\nBons estudos.`);
